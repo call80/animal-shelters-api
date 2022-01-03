b2131394df53bfb2e7c290cd898ffed2c0b55ebf
My Project <you@example.com>
2022-01-03 16:10:14 +0200

    Initial commit

---
+++ src/main/java/com/p5/adoptions/SheltersApiApplication.java
@@ -0,0 +1,13 @@
+package com.p5.adoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheltersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheltersApiApplication.class, args);
+	}
+
+}
